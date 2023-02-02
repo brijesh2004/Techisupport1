2491efe002263236ee17fc548561363055b5b1d2
brijesh2004 <brskumar0102@gmail.com>
2023-02-03 00:25:22 +0530

    adding sourcecode

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
+require("./db/conn");
+const Register = require("./models/register");
 const path = require("path")
 const hbs = require('hbs');
 var nodemailer = require('nodemailer');
@@ -16,6 +18,10 @@ const partial_path = path.join(__dirname, "../templates/partials");
 
 app.set('view engine', 'hbs');
 app.set('views', template_path);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 // hbs.registerPartial('headPartial', 'navbar');  
 hbs.registerPartials(partial_path);
 
@@ -33,6 +39,56 @@ app.get("/about", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render("login");
+})
+
+// register the student
+
+app.post("/RegisterStudent" , async(req,res)=>{
+    try{
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const cpassword = req.body.confirmPassword;
+        // console.log(name + " "+ email +" "+  password +" "+ cpassword);
+        // console.log("hello world" +name);
+        if(password=== cpassword){
+            const registerStudent = new Register({
+                Fullname: name,
+                email: email,
+                password:password ,
+                confirmpassword:cpassword
+            })
+            const registed =  await registerStudent.save();
+           res.status(201).render("about");
+        }
+        else{
+            res.send("password are not matching")
+        }
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+})
+
+// login the user 
+app.post("/loginStudent", async(req,res) =>{
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        // console.log(email + " "+ password);
+        const userEmail = await Register.findOne({email:email});
+        // res.send(userEmail);
+        // console.log(userEmail);
+        if(userEmail.password === password){
+            res.status(201).render("about");
+        }
+        else{
+            res.send("invalid user");
+        }
+
+    } catch (error) {
+        res.status(400).send("Invalid user ");
+    }
 })
 
 app.get("/sourcecode", (req, res) => {
@@ -102,6 +158,28 @@ app.get("/templates/views/sourceCode/MovingSquare", (req, res) => {
 app.get("/templates/views/sourceCode/DivMoveWithCursor", (req, res) => {
     res.render("sourceCode/DivMoveWithCursor");
 })
+app.get("/templates/views/sourceCode/SumValidator", (req, res) => {
+    res.render("sourceCode/SumValidator");
+})
+app.get("/templates/views/sourceCode/ExpandingCards", (req, res) => {
+    res.render("sourceCode/ExpandingCards");
+})
+app.get("/templates/views/sourceCode/hoberBox", (req, res) => {
+    res.render("sourceCode/hoberBox");
+})
+app.get("/templates/views/sourceCode/Pop_balloon", (req, res) => {
+    res.render("sourceCode/Pop_balloon");
+})
+app.get("/templates/views/sourceCode/digitalClock", (req, res) => {
+    res.render("sourceCode/digitalClock");
+})
+app.get("/templates/views/sourceCode/toggleDarkMode", (req, res) => {
+    res.render("sourceCode/toggleDarkMode");
+})
+
+
+
+
 
 app.get("/templates/views/sourceCode/CssWave", (req, res) => {
     res.render("sourceCode/CssWave");
